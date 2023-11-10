0b5ac92e0bafff2d7a496fd4e8799d4beb380db9
romanmtz <113059087+romanmtz@users.noreply.github.com>
2023-11-10 00:09:54 -0600

    Designed Post Form

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     }
     return (
         <>
-            <h1>This is React WebApp </h1>
+           {/* /* <h1>This is React WebApp</h1>
             <form action="">
                 <input type="text" placeholder="name"
                 value={name} onChange={(e) => setName(e.target.value)} />
@@ -30,7 +30,33 @@ function App() {
                 value={email} onChange={(e) => setEmail(e.target.value)} />
                 <button type="submit"
                 onClick={handleOnSubmit}>submit</button>
+            </form> */ }
+            
+           
+            
+            
+            <img class="logo" src="ephemere.svg"></img>
+            
+            
+            <form class="post" action="">
+                <textarea maxLength="300"name="text" placeholder='Send A Letter.' required></textarea>
+
+                    <div>
+                    <input maxLength="30" name="from" placeholder='From:'required></input>
+                    <input maxLength="30" name="to" placeholder='To:'required></input>
+                    <button>Send</button>
+                    </div>
+                
             </form>
+            
+            
+            
+            
+            
+            
+            
+            
+            
  
         </>
     );
